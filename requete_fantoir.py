ce7d50930dea28b6d85900a5710c046945abe831
vdct <vdct@laposte.net>
2014-10-05 23:38:57 +0200

    Restriction aux voies avec adresses

--- requete_fantoir.py
+++ requete_fantoir.py
@@ -10,32 +10,22 @@ from pg_connexion import get_pgc
 
 cgitb.enable()
 def get_data_from_pg(data_type,insee_com):
-	a_res = []
 	fq = open('sql/{:s}.sql'.format(data_type),'rb')
-	str_raw_query = fq.read().replace('__com__',insee_com)
+	str_query = fq.read().replace('__com__',insee_com)
 	fq.close()
-	str_query = str_raw_query.replace('__null__clause__','NULL')
 	cur = pgc.cursor()
 	cur.execute(str_query)
 	res = cur.fetchall()
-	a_res.append(res)
-	str_query = str_raw_query.replace('__null__clause__','NOT NULL')
-	cur = pgc.cursor()
-	cur.execute(str_query)
-	res = cur.fetchall()
-	a_res.append(res)
-	
-	# res = []
-	# for lt in cur:
-		# res.append(eval(list(lt)))
 		
-	return a_res
+	return res
 	
 print "Content-Type: application/json"
 print ""
 
 pgc = get_pgc()
 params = cgi.FieldStorage()
-a = json.JSONEncoder().encode(get_data_from_pg('voie_insee',params['insee'].value))
+data = [get_data_from_pg('voies_adresses_non_rapprochees_insee',params['insee'].value),get_data_from_pg('voies_adresses_rapprochees_insee',params['insee'].value)]
+
+a = json.JSONEncoder().encode(data)
 
 print(a)
