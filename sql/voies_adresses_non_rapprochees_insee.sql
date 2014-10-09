87ce1f96da81c24478b54124e007564d9ec840fa
vdct <vdct@laposte.net>
2014-10-09 13:52:11 +0200

    Edition des voies non rapprochées

--- sql/voies_adresses_non_rapprochees_insee.sql
+++ sql/voies_adresses_non_rapprochees_insee.sql
@@ -1,6 +1,8 @@
 SELECT	f.code_insee||f.id_voie||f.cle_rivoli fantoir,
 		nature_voie||' '||libelle_voie voie,
-		'--'
+		'--',
+		st_x(g.geometrie),
+		st_y(g.geometrie)
 FROM	fantoir_voie f
 JOIN	(SELECT	fantoir
 		FROM	cumul_adresses
@@ -13,6 +15,12 @@ FROM	cumul_adresses
 WHERE	insee_com = '__com__'	AND
 		source = 'OSM') j
 ON		f.code_insee||f.id_voie||f.cle_rivoli = j.fantoir
+JOIN	(SELECT DISTINCT fantoir,
+				FIRST_VALUE(geometrie) OVER(PARTITION BY fantoir) geometrie
+		FROM	cumul_adresses
+		WHERE	insee_com = '__com__' AND
+				source = 'CADASTRE') g
+ON		g.fantoir = j.fantoir
 WHERE	f.code_insee = '__com__'	AND
 		f.type_voie = '1'			AND
 		f.date_annul = '0000000'
