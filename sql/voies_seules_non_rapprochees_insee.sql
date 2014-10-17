3d4af98f61d73d9510b757863a6885feed56c2c0
vdct <vdct@laposte.net>
2014-10-17 09:24:06 +0200

    liens visu et edition pour les voies non rapprochees sans adresses

--- sql/voies_seules_non_rapprochees_insee.sql
+++ sql/voies_seules_non_rapprochees_insee.sql
@@ -1,6 +1,8 @@
 SELECT	f.code_insee||f.id_voie||f.cle_rivoli fantoir,
-		nature_voie||' '||libelle_voie voie,
-		'--'
+		f.nature_voie||' '||f.libelle_voie voie,
+		'--',
+		st_x(g.geometrie),
+		st_y(g.geometrie)
 FROM	fantoir_voie f
 LEFT OUTER JOIN	(SELECT	fantoir
 				FROM	cumul_adresses
@@ -12,6 +14,12 @@ LEFT OUTER JOIN	(SELECT	fantoir
 				WHERE	insee_com = '__com__'	AND
 						source = 'OSM') o
 ON		f.code_insee||f.id_voie||f.cle_rivoli = o.fantoir
+LEFT OUTER JOIN	(SELECT	voie_cadastre,
+						ST_Transform(ST_SetSRID(ST_Centroid(ST_Collect(ST_Centroid(geometrie))),900913),4326) geometrie
+				FROM	parcelles
+				WHERE	insee_com = '__com__'
+				GROUP BY	1)g
+ON		f.nature_voie||' '||f.libelle_voie = g.voie_cadastre
 WHERE	f.code_insee = '__com__'	AND
 		f.type_voie = '1'			AND
 		f.date_annul = '0000000'	AND
