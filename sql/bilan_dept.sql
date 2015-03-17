f6f47182a510e30bc5ca4f7dc87602dee003c3c8
vdct <vdct@laposte.net>
2015-03-17 23:23:16 +0100

    Bilan par départment - ajout des noms de communes

--- sql/bilan_dept.sql
+++ sql/bilan_dept.sql
@@ -1,12 +1,14 @@
 WITH v AS (SELECT insee_com,count(distinct fantoir) voies_rapprochees FROM cumul_voies WHERE insee_com like '38___' GROUP BY insee_com),
 t AS (SELECT code_insee,count(*) voies_fantoir FROM fantoir_voie WHERE code_insee like '38___' AND type_voie in ('1','2') GROUP BY code_insee)
 --tl AS (SELECT code_insee,count(*) voies_fantoir FROM fantoir_voie WHERE code_insee like '38___' AND type_voie in ('1','2','3') GROUP BY code_insee)
-SELECT v.insee_com "Code INSEE",v.voies_rapprochees "Voies rapprochées",t.voies_fantoir "Voies FANTOIR",((v.voies_rapprochees*100/t.voies_fantoir))::integer "Pourcentage de rapprochement"
+SELECT v.insee_com "Code INSEE",c.nom "Commune",v.voies_rapprochees "Voies rapprochées",t.voies_fantoir "Voies FANTOIR",((v.voies_rapprochees*100/t.voies_fantoir))::integer "Pourcentage de rapprochement"
 --tl.voies_fantoir "Voies et lieux-dits FANTOIR"
 FROM v
+JOIN communes c
+ON v.insee_com = c.insee
 JOIN t
 ON v.insee_com = t.code_insee
 --JOIN tl
 --ON v.insee_com = tl.code_insee
-ORDER  BY 4
+ORDER  BY 5
 --ORDER BY (voies_rapprochees / t.voies_fantoir) desc
