f17b240d6cc44cf88f4e52073ce0e866c8138717
vdct <vdct@laposte.net>
2015-04-05 19:17:56 +0200

    Format du cadastre (#12)

--- sql/stats_dept.sql
+++ sql/stats_dept.sql
@@ -1,4 +1,11 @@
 WITH 
+r AS (	SELECT	insee_com,
+				CASE format_cadastre
+					WHEN 'IMAG' THEN 'Raster'
+					ELSE 'Vecteur'
+				END format_cadastre
+		FROM	code_cadastre
+		WHERE	insee_com like '__dept__'),
 a AS (	SELECT 	insee_com,
 				count(distinct fantoir) voies_avec_adresses_rapprochees
 		FROM 	cumul_adresses
@@ -21,7 +28,8 @@ f AS (	SELECT 	code_insee,
 		WHERE	code_insee like '__dept__'	AND
 				type_voie in ('1','2','3')
 		GROUP BY code_insee)
-SELECT 	c.insee, --Code INSEE
+SELECT 	r.format_cadastre,
+		c.insee, --Code INSEE
 		c.nom, --Commune
 		coalesce(a.voies_avec_adresses_rapprochees::integer,0), --Voies avec adresses rapprochées
 		coalesce(v.voies_rapprochees::integer,0), --Toutes voies rapprochées
@@ -39,4 +47,6 @@ JOIN 	t
 ON 		c.insee = t.code_insee
 JOIN 	f
 ON 		c.insee = f.code_insee
-ORDER  BY 6
+JOIN 	r
+ON 		c.insee = r.insee_com
+ORDER  BY 2
