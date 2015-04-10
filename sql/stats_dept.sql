b43a593adb7051fd1880204e7cef2c61d931bacb
vdct <vdct@laposte.net>
2015-04-10 22:54:17 +0200

    Comptage distinct des rapprochements sur lieux-dits, suppr. Fantoirs vides(#12)

--- sql/stats_dept.sql
+++ sql/stats_dept.sql
@@ -9,13 +9,26 @@ r AS (	SELECT	insee_com,
 a AS (	SELECT 	insee_com,
 				count(distinct fantoir) voies_avec_adresses_rapprochees
 		FROM 	cumul_adresses
-		WHERE	insee_com like '__dept__' and voie_osm IS NOT NULL and voie_osm != ''
+		WHERE	insee_com like '__dept__'	AND
+				voie_osm IS NOT NULL and voie_osm != ''
 		GROUP BY insee_com),
 v AS (	SELECT 	insee_com,
 				count(distinct fantoir) voies_rapprochees
 		FROM 	cumul_voies
-		WHERE	insee_com like '__dept__' and voie_osm IS NOT NULL
+		WHERE	insee_com like '__dept__'	AND
+				fantoir != ''				AND
+				voie_osm IS NOT NULL
 		GROUP BY insee_com),
+vl AS (	SELECT 	c.insee_com,
+				count(distinct c.fantoir) voies_rapprochees
+		FROM 	cumul_voies c
+		JOIN	fantoir_voie f
+		ON		c.fantoir = f.code_insee||f.id_voie||f.cle_rivoli 
+		WHERE	c.insee_com like '__dept__'	AND
+				c.fantoir != ''				AND
+				c.voie_osm IS NOT NULL		AND
+				f.type_voie = '3'
+		GROUP BY c.insee_com),
 t AS (	SELECT 	code_insee,
 				count(*) voies_fantoir
 		FROM 	fantoir_voie
@@ -31,8 +44,9 @@ f AS (	SELECT 	code_insee,
 i AS (	SELECT 	r.format_cadastre,
 				c.insee, --Code INSEE
 				c.nom, --Commune
-				coalesce(a.voies_avec_adresses_rapprochees::integer,0) a, --Voies avec adresses rapprochées 	(a)
+				coalesce(a.voies_avec_adresses_rapprochees::integer,0) a, --Voies avec adresses rapprochées (a)
 				coalesce(v.voies_rapprochees::integer,0) b, --Toutes voies rapprochées						(b)
+				coalesce(vl.voies_rapprochees::integer,0) b1, --Voies rapprochées sur lieux-dits			(bl)
 				t.voies_fantoir c, --Voies FANTOIR 															(c)
 				f.voies_fantoir d, --Voies FANTOIR + lieux-dits												(d)
 				coalesce(((a.voies_avec_adresses_rapprochees*100/t.voies_fantoir))::integer,0), --Pourcentage de rapprochement avec adresses
@@ -41,6 +55,8 @@ i AS (	SELECT 	r.format_cadastre,
 		FROM	communes c
 		LEFT OUTER JOIN	v
 		ON 		c.insee = v.insee_com
+		LEFT OUTER JOIN	vl
+		ON 		c.insee = vl.insee_com
 		LEFT OUTER JOIN a
 		ON 		c.insee = a.insee_com
 		JOIN 	t
