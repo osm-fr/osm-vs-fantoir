7c233748b71a969caa98c69a1e9724dc5c56b12a
Vincent de Château-Thierry <vdct@laposte.net>
2019-11-13 17:23:32 +0000

    adresses sur voies non rapprochées (#45)

--- sql/numeros_hors_osm_par_fantoir.sql
+++ sql/numeros_hors_osm_par_fantoir.sql
@@ -1,15 +1,15 @@
 WITH
 diff
 AS
-(SELECT numero,fantoir,voie_osm,insee_com FROM cumul_adresses WHERE insee_com = '__com__' AND fantoir = '__fantoir__' AND source = 'CADASTRE' 
+(SELECT numero,fantoir,insee_com FROM cumul_adresses WHERE insee_com = '__com__' AND fantoir = '__fantoir__' AND source = 'CADASTRE' 
 EXCEPT
-SELECT numero,fantoir,voie_osm,insee_com FROM cumul_adresses WHERE insee_com = '__com__' AND fantoir = '__fantoir__' AND  source = 'OSM')
+SELECT numero,fantoir,insee_com FROM cumul_adresses WHERE insee_com = '__com__' AND fantoir = '__fantoir__' AND  source = 'OSM')
 SELECT ST_X(c.geometrie),
        ST_Y(c.geometrie),
        diff.numero,
        diff.fantoir,
-       diff.voie_osm
+       COALESCE(c.voie_osm,c.voie_cadastre)
 FROM   cumul_adresses c
 JOIN   diff
-USING  (numero,fantoir,voie_osm,insee_com)
+USING  (numero,fantoir,insee_com)
 WHERE  source = 'CADASTRE';
--- sql/voies_adresses_non_rapprochees_insee.sql
+++ sql/voies_adresses_non_rapprochees_insee.sql
@@ -1,9 +1,19 @@
+WITH
+diff_numero_fantoir
+AS
+(SELECT numero,fantoir FROM cumul_adresses WHERE insee_com = '__com__' and source = 'CADASTRE'
+EXCEPT
+SELECT numero,fantoir FROM cumul_adresses WHERE insee_com = '__com__' and source = 'OSM'),
+fantoir_numeros_manquants
+AS
+(SELECT DISTINCT fantoir,count(*) AS a_proposer FROM diff_numero_fantoir GROUP BY 1)
 SELECT	f.code_insee||f.id_voie||f.cle_rivoli fantoir,
 		nature_voie||' '||libelle_voie voie,
 		'--',
 		st_x(g.geometrie),
 		st_y(g.geometrie),
-		COALESCE(s.id_statut,0)
+		COALESCE(s.id_statut,0),
+		COALESCE(fm.a_proposer,0)
 FROM	fantoir_voie f
 JOIN	(SELECT	fantoir
 		FROM	cumul_adresses
@@ -22,6 +32,8 @@ JOIN	(SELECT DISTINCT fantoir,
 		WHERE	insee_com = '__com__' AND
 				source = 'CADASTRE') g
 ON		g.fantoir = j.fantoir
+LEFT OUTER JOIN fantoir_numeros_manquants fm
+ON      f.code_insee||f.id_voie||f.cle_rivoli = fm.fantoir
 LEFT OUTER JOIN	(SELECT fantoir,id_statut
 				FROM 	(SELECT	*,rank() OVER (PARTITION BY fantoir ORDER BY timestamp_statut DESC) rang
 						FROM 	statut_fantoir
