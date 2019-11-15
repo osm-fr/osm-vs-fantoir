cf1a5fd08e05b7ab259148ec73b0eb1cbaf36890
Vincent de Château-Thierry <vdct@laposte.net>
2019-11-15 20:32:19 +0000

    Migration de statut_fantoir

--- statut_fantoir.py
+++ statut_fantoir.py
@@ -1,30 +1,25 @@
-#!/usr/bin/env python
+#!./venv37/bin/python
 # -*- coding: utf-8 -*-
 
 import cgi
 import cgitb
 import json
 import sys
-sys.path.append('/data/project/cadastre.openstreetmap.fr/bin/cadastre-housenumber/bano')
-from pg_connexion import get_pgc
 
+import db
 
 cgitb.enable()
-print "Content-Type: application/json"
-print ""
+print("Content-Type: application/json\n")
 
-pgc = get_pgc()
 params = cgi.FieldStorage()
 insee_com = params['insee'].value
 statut = params['statut'].value
 fantoir = params['fantoir'].value
 
 try:
-	str_insert = "INSERT INTO statut_fantoir VALUES ('{:s}',{:s},(select extract(epoch from now()))::integer,'{:s}');COMMIT;".format(fantoir,statut,insee_com);
-	cur = pgc.cursor()
-	cur.execute(str_insert)
+    with db.bano.cursor() as cur:
+        cur.execute(f"INSERT INTO statut_fantoir VALUES ('{fantoir}',{statut},(SELECT EXTRACT(epoch from now())::integer),'{insee_com}');COMMIT;")
 except:
 	statut = -1
 
-a = json.JSONEncoder().encode(statut)
-print(a)
+print(json.JSONEncoder().encode(statut))
