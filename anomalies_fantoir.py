a4e8e7c2a4c71a700cc26cf35bfcffb50e600b40
Vincent de Château-Thierry <vdct@laposte.net>
2019-12-15 09:30:29 +0000

    Anomalies Fantoir : points (WIP #71)

---
+++ anomalies_fantoir.py
@@ -0,0 +1,33 @@
+#!./venv37/bin/python
+# -*- coding: utf-8 -*-
+
+import cgi
+import cgitb
+# import os.path
+import sys
+import json
+
+import db
+
+def get_data_from_pg(conn,data_type,substitutions):
+    with conn.cursor() as cur:
+        with open(f"sql/{data_type}.sql",'r') as fq:
+            str_query = fq.read()
+            for s in substitutions:
+                str_query = str_query.replace(s[0],s[1])
+            # print(str_query)
+            cur.execute(str_query)
+            return cur.fetchall()
+
+params = cgi.FieldStorage()
+dept = params['dept'].value
+# dept = '29'
+
+data_point = get_data_from_pg(db.bano_cache,'anomalies_fantoir_point',[['__dept__',dept]])
+# data_ligne = get_data(db.bano_cache,'anomalies_fantoir_ligne',[[dept,'__dept__']])
+# data_polygone = get_data(db.bano_cache,'anomalies_fantoir_polygone',[[dept,'__dept__']])
+# data_relation = get_data(db.bano_cache,'anomalies_fantoir_relation',[[dept,'__dept__']])
+
+print("Content-Type: application/json\n")
+
+print(json.JSONEncoder().encode(data_point))
---
+++ sql/anomalies_fantoir_point.sql
@@ -0,0 +1,33 @@
+WITH
+f
+AS
+(SELECT pt."ref:FR:FANTOIR" FROM planet_osm_point pt
+JOIN planet_osm_polygon p
+ON	ST_Intersects(pt.way, p.way) AND pt.way && p.way
+WHERE pt."ref:FR:FANTOIR" != '' AND
+      p."ref:INSEE" = '__dept__' AND
+      p.boundary = 'administrative' AND
+      p.admin_level = 6
+EXCEPT
+SELECT fantoir10 FROM fantoir_voie
+WHERE code_dept = '__dept__')
+SELECT	p."ref:INSEE",
+		p.name,
+		ST_X(ST_Transform(pt.way,4326)) x,
+        ST_Y(ST_Transform(pt.way,4326)) y,
+		pt.osm_id,
+		CASE
+		    WHEN pt.name = '' THEN pt."addr:housenumber"||' '||pt."addr:street"
+		    ELSE pt.name
+		END AS name,
+		pt."ref:FR:FANTOIR"
+FROM    planet_osm_point pt
+JOIN    f
+USING   ("ref:FR:FANTOIR")
+JOIN    planet_osm_polygon p
+ON	    ST_Intersects(pt.way, p.way) AND pt.way && p.way
+WHERE   p.boundary = 'administrative' AND
+        (p.admin_level=8 OR (p.admin_level=9 AND (p."ref:INSEE" LIKE '751__' OR
+                                                  p."ref:INSEE" LIKE '6938_' OR
+                                                  p."ref:INSEE" LIKE '132__' )));
+
