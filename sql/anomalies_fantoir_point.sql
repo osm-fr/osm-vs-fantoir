31db50c0801a817b8935f96ba5697b6e9b65e06d
Vincent de Château-Thierry <vdct@laposte.net>
2019-12-15 11:43:24 +0000

    Affichage du nombre de lignes + fix (#71)

--- sql/anomalies_fantoir_point.sql
+++ sql/anomalies_fantoir_point.sql
@@ -27,7 +27,7 @@ USING   ("ref:FR:FANTOIR")
 JOIN    planet_osm_polygon p
 ON	    ST_Intersects(pt.way, p.way) AND pt.way && p.way
 WHERE   p.boundary = 'administrative' AND
-        (p.admin_level=8 OR (p.admin_level=9 AND (p."ref:INSEE" LIKE '751__' OR
+        ((p.admin_level=8 AND p."ref:INSEE" NOT IN ('13055','69123','75056')) OR (p.admin_level=9 AND (p."ref:INSEE" LIKE '751__' OR
                                                   p."ref:INSEE" LIKE '6938_' OR
                                                   p."ref:INSEE" LIKE '132__' )));
 
