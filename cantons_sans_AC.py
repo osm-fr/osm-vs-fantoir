b8296942cbcf2c25fb4c8c99f5f190845e981a3a
Vincent de Château-Thierry <vdct@laposte.net>
2020-01-04 15:58:41 +0000

    page de correction des admin_centre de canton (#75)

---
+++ cantons_sans_AC.py
@@ -0,0 +1,23 @@
+#!./venv37/bin/python
+# -*- coding: utf-8 -*-
+
+import cgi
+import cgitb
+import os.path
+import sys
+import json
+
+import db
+
+def get_data(data_type):
+	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'sql/{:s}.sql'.format(data_type)),'r') as fq:
+		with db.bano_cache.cursor() as cur:
+			cur.execute(fq.read())
+
+			return cur.fetchall()
+
+data = get_data('cantons_sans_AC')
+
+print("Content-Type: application/json\n")
+
+print(json.JSONEncoder().encode(data))
