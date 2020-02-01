5329387dbdaf560035648f43f1d921b999446eab
Vincent de Château-Thierry <vdct@laposte.net>
2020-02-01 21:02:25 +0000

    Rapprochement des voies avec adresses - élargissement aux données OD (#86)

--- sql/voies_adresses_rapprochees_insee.sql
+++ sql/voies_adresses_rapprochees_insee.sql
@@ -7,7 +7,7 @@ SELECT numero,fantoir FROM cumul_adresses WHERE insee_com = '__com__' and source
 fantoir_numeros_manquants
 AS
 (SELECT DISTINCT fantoir,count(*) AS a_proposer FROM diff_numero_fantoir GROUP BY 1)
-SELECT	f.code_insee||f.id_voie||f.cle_rivoli fantoir,
+SELECT	f.fantoir10 fantoir,
 		nature_voie||' '||libelle_voie voie,
 		j.voie_osm,
 		st_x(g.geometrie),
@@ -18,13 +18,13 @@ FROM	fantoir_voie f
 JOIN 	(SELECT DISTINCT fantoir,voie_osm
 		FROM	cumul_adresses
 		WHERE	insee_com = '__com__' AND
-				source in ('CADASTRE','OSM')) j
-ON		f.code_insee||f.id_voie||f.cle_rivoli = j.fantoir
+		        COALESCE(voie_osm,'') != '') j
+ON		f.fantoir10 = j.fantoir
 JOIN	(SELECT DISTINCT fantoir,
 				FIRST_VALUE(geometrie) OVER(PARTITION BY fantoir) geometrie
 		FROM	cumul_adresses
 		WHERE	insee_com = '__com__') g
-ON		f.code_insee||f.id_voie||f.cle_rivoli = g.fantoir
+ON		f.fantoir10 = g.fantoir
 LEFT OUTER JOIN	(SELECT fantoir,id_statut
 				FROM 	(SELECT	*,rank() OVER (PARTITION BY fantoir ORDER BY timestamp_statut DESC) rang
 						FROM 	statut_fantoir
@@ -32,9 +32,8 @@ LEFT OUTER JOIN	(SELECT fantoir,id_statut
 				WHERE	rang = 1) s
 ON		j.fantoir = s.fantoir
 LEFT OUTER JOIN fantoir_numeros_manquants fm
-ON      f.code_insee||f.id_voie||f.cle_rivoli = fm.fantoir
+ON      f.fantoir10 = fm.fantoir
 WHERE	f.code_insee = '__com__'	AND
 		f.type_voie in ('1','2')	AND
-		f.date_annul = '0000000'	AND
-		COALESCE(j.voie_osm,'') != ''
+		f.date_annul = '0000000'		
 ORDER BY 3;
