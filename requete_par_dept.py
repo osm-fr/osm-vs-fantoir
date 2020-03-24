a64d3b43a45fbe27cdd2a44ccc8358ad2b58cf94
Vincent de Château-Thierry <vdct@laposte.net>
2020-03-24 13:42:31 +0000

    Top des adresses manquantes (#102)

--- requete_par_dept.py
+++ requete_par_dept.py
@@ -9,18 +9,27 @@ import json
 
 import db
 
-def get_data(data_type,dept):
+def get_data(data_type,dept,cache_db):
+    if cache_db:
+        dbconn = db.bano_cache
+    else:
+        dbconn = db.bano
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'sql/{:s}.sql'.format(data_type)),'r') as fq:
-        with db.bano_cache.cursor() as cur:
+        with dbconn.cursor() as cur:
             cur.execute(fq.read().replace('__dept__',dept))
 
             return cur.fetchall()
 
+cache = True
 params = cgi.FieldStorage()
 dept = params['dept'].value
 requete = params['requete'].value
+
+if 'cache' in params:
+    cache = not(params['cache'].value.lower() == 'false')
+
+# requete = 'top_adresses_manquantes'
 # dept = '92'
-# requete = 'maj_population_2017'
 
 print("Content-Type: application/json\n")
-print(json.JSONEncoder().encode([get_data('infos_dept',dept),get_data(requete,dept)]))
+print(json.JSONEncoder().encode([get_data('infos_dept',dept,True),get_data(requete,dept,cache)]))
