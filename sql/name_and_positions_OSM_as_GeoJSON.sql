383246e4ffb07fda4034fddc0013646ee50cae4d
Vincent de Château-Thierry <vdct@laposte.net>
2020-11-11 22:57:18 +0000

    Adaptation BAN pour les adresses en Relations à intégrer (#110)

--- sql/name_and_positions_OSM_as_GeoJSON.sql
+++ sql/name_and_positions_OSM_as_GeoJSON.sql
@@ -1,4 +1,4 @@
-SELECT	DISTINCT COALESCE(voie_osm,voie_cadastre),
+SELECT	DISTINCT COALESCE(voie_osm,voie_autre),
                  ST_AsGeoJSON(geometrie)
 FROM    cumul_adresses
 WHERE   insee_com = '__com__'	AND
