d8eb6449aa2b5cd6b44f4c6dab040a9474dde7e0
Vincent de Château-Thierry <vdct@laposte.net>
2020-11-22 18:21:38 +0000

    Nom des communes post-fusion (#111)

--- sql/infos_commune_insee.sql
+++ sql/infos_commune_insee.sql
@@ -2,8 +2,10 @@ SELECT name,
 		ST_X(p),
 		ST_Y(p)
 FROM	(SELECT	name,
-				ST_Transform(ST_Centroid(way),4326) p
+				ST_Transform(ST_Centroid(way),4326) p,
+				admin_level
 		FROM	planet_osm_polygon
 		WHERE	boundary='administrative' AND
                 admin_level in (8,9) AND
-                "ref:INSEE" = '__com__')a;
+                "ref:INSEE" = '__com__'
+        ORDER BY admin_level)a;
