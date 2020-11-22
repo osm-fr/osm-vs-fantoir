3f8bb92e95abf55d04b4688e92cbb928bd31ff84
Vincent de Château-Thierry <vdct@laposte.net>
2020-11-22 17:26:24 +0000

    Ménage ds les metadonnées après bascule Cadastre > BAN

--- requete_fantoir.py
+++ requete_fantoir.py
@@ -58,8 +58,6 @@ def main():
     tab = int(params.getvalue('tab',0))
     dept = hp.get_code_dept_from_insee(insee_com)
 
-    labels_statuts_fantoir = get_data_from_bano('labels_statuts_fantoir','')
-
     infos_commune = get_data_from_bano_cache('infos_commune_insee',insee_com)
     if infos_commune:
         nom_commune = infos_commune[0][0]
@@ -71,20 +69,16 @@ def main():
         lat_commune = None
     a_voisins = [[v[0],v[1],v[2]] for v in get_data_from_bano_cache('voisins_insee',insee_com)]
 
-    date_import_cadastre = get_batch_infos_etape_dept('loadDeptBal',dept, 'CADASTRE')
     date_fin_cumul = ['','']
     fin_etape = get_batch_infos_etape_commune('loadCumul',insee_com)
     if len(fin_etape) == 1:
         date_fin_cumul = [[],fin_etape[0]]
     elif fin_etape:
         date_fin_cumul = fin_etape
-    date_cache_hsnr = '' #get_fin_etape_dept('cache_dept_hsnr_insee',dept)[0]
-    date_cache_highway = '' #get_fin_etape_dept('cache_dept_highway_insee',dept)[0]
-    date_cache_highway_relation = ''#get_fin_etape_dept('cache_dept_highway_relation_insee',dept)[0]
 
     data_columns = [get_data_from_bano('voies_adresses_non_rapprochees_insee',insee_com),get_data_from_bano('voies_adresses_rapprochees_insee',insee_com),get_data_from_bano('voies_seules_non_rapprochees_insee',insee_com),get_data_from_bano('voies_seules_rapprochees_insee',insee_com),get_data_from_bano('places_non_rapprochees_insee',insee_com),get_data_from_bano('places_rapprochees_insee',insee_com),get_data_from_bano('voies_OSM_non_rapprochees_insee',insee_com)]
 
-    data = [[nom_commune,date_import_cadastre,date_fin_cumul[0],date_fin_cumul[1],date_cache_hsnr,date_cache_highway,date_cache_highway_relation,lon_commune,lat_commune,labels_statuts_fantoir,a_voisins],*data_columns]
+    data = [[nom_commune,date_fin_cumul[0],date_fin_cumul[1],lon_commune,lat_commune,a_voisins],*data_columns]
 
     if format == 'csv':
         print(f'Content-Type: text/csv\nContent-Disposition: Attachment; filename="insee-{insee_com}-{nom_commune}-onglet {tab}.csv"\n')
