707115f1653ca385aeb28a70477368fed9897401
Jérôme Amagat <jerome.amagat@gmail.com>
2020-12-19 02:58:54 +0100

    ajout stats sur nbre adresses dans stats_dept

--- sql/stats_dept.sql
+++ sql/stats_dept.sql
@@ -13,6 +13,21 @@ a AS (	SELECT 	insee_com,
 		WHERE	insee_com like '__dept__%'	AND
 				COALESCE(voie_osm,'') != ''
 		GROUP BY insee_com),
+adrOSM AS (	SELECT 	insee_com,
+				count(distinct concat(numero,fantoir,voie_osm)) adresses_OSM
+		FROM 	cumul_adresses
+		WHERE	insee_com like '__dept__%'
+		GROUP BY insee_com),
+adrBAN AS (	SELECT 	insee_com,
+				count(distinct concat(numero,fantoir,voie_autre)) adresses_BAN
+		FROM 	cumul_adresses
+		WHERE	insee_com like '__dept__%'
+		GROUP BY insee_com),
+adrnon AS (	SELECT 	insee_com,
+				count(distinct concat(numero,fantoir,voie_autre)) adresses_non_rapprochees
+		FROM 	cumul_adresses
+		WHERE	insee_com like '__dept__%' AND COALESCE(voie_osm,'') = ''
+		GROUP BY insee_com),
 v AS (	SELECT 	insee_com,
 				count(distinct fantoir) voies_rapprochees
 		FROM 	cumul_voies
@@ -46,17 +61,24 @@ i AS (	SELECT 	r.format_cadastre,
 				r.insee_com, --Code INSEE
 				r.nom_com, --Commune
 				coalesce(a.voies_avec_adresses_rapprochees::integer,0) a, --Voies avec adresses rapprochées (a)
-				coalesce(v.voies_rapprochees::integer,0) b, --Toutes voies rapprochées						(b)
-				coalesce(vl.voies_rapprochees::integer,0) b1, --Voies rapprochées sur lieux-dits			(bl)
-				t.voies_fantoir c, --Voies FANTOIR 															(c)
-				f.voies_fantoir d, --Voies FANTOIR + lieux-dits												(d)
+				coalesce(v.voies_rapprochees::integer,0) b, --Toutes voies rapprochées (b)
+				coalesce(vl.voies_rapprochees::integer,0) b1, --Voies rapprochées sur lieux-dits (bl)
+				t.voies_fantoir c, --Voies FANTOIR (c)
+				f.voies_fantoir d, --Voies FANTOIR + lieux-dits (d)
 				coalesce(((a.voies_avec_adresses_rapprochees*100/t.voies_fantoir))::integer,0), --Pourcentage de rapprochement avec adresses
 				coalesce(((v.voies_rapprochees*100/t.voies_fantoir))::integer,0), --Pourcentage de rapprochement sur voies
-				coalesce(((v.voies_rapprochees*100/f.voies_fantoir))::integer,0) --Pourcentage de rapprochement sur voies+lieux-dits
+				coalesce(((v.voies_rapprochees*100/f.voies_fantoir))::integer,0), --Pourcentage de rapprochement sur voies+lieux-dits
+				coalesce(adrOSM.adresses_OSM::integer,0), --Adresses OSM
+				coalesce(adrBAN.adresses_BAN::integer,0), --Adresses BAN, BAL
+				coalesce(adrnon.adresses_non_rapprochees::integer,0), --Adresses sans voie rapprochée
+				coalesce(((100-adrnon.adresses_non_rapprochees*100/adrBAN.adresses_BAN))::integer,100) --Pourcentage d'adresses avec voie rapprochée
 		FROM	r
-		LEFT OUTER JOIN	v USING (insee_com)
-		LEFT OUTER JOIN	vl USING (insee_com)
+		LEFT OUTER JOIN v USING (insee_com)
+		LEFT OUTER JOIN vl USING (insee_com)
 		LEFT OUTER JOIN a USING (insee_com)
+		LEFT OUTER JOIN adrOSM USING (insee_com)
+		LEFT OUTER JOIN adrBAN USING (insee_com)
+		LEFT OUTER JOIN adrnon USING (insee_com)
 		JOIN 	t USING (insee_com)
 		JOIN 	f USING (insee_com))
 SELECT	i.*,--(((a/c::double precision)*(c-a)) + ((b/c::double precision)*(c-b)) + ((b/d::double precision)* (d-b)))::integer
