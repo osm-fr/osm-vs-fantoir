a0a3f38919af2899ab13c4b39c5b9a15160eddce
Vincent de Château-Thierry <vdct@laposte.net>
2021-05-21 22:16:36 +0000

    Tolérance de recherche pour la géométrie des rues (#145)

--- sql/name_and_positions_OSM_as_GeoJSON.sql
+++ sql/name_and_positions_OSM_as_GeoJSON.sql
@@ -1,5 +1,5 @@
 SELECT	DISTINCT COALESCE(voie_osm,voie_autre),
-                 ST_AsGeoJSON(geometrie)
+                 ST_AsGeoJSON(ST_Buffer(geometrie,0.0001))
 FROM    cumul_adresses
 WHERE   insee_com = '__com__'	AND
 		fantoir = '__fantoir__';
