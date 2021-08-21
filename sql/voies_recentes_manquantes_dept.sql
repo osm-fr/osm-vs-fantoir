97eb960e3d7751dfd579e88b8cd6e57ade212076
Vincent de Château-Thierry <vdct@laposte.net>
2021-08-21 21:52:04 +0000

    Requêtes lentes - voies récentes (#118)

--- sql/voies_recentes_manquantes_dept.sql
+++ sql/voies_recentes_manquantes_dept.sql
@@ -1,57 +1,86 @@
-WITH
 -- statut FANTOIR ---------------------
-aa AS
-(SELECT *,
+CREATE TEMP TABLE statut_fantoir
+AS
+(SELECT fantoir
+FROM    (SELECT *,
         RANK() OVER(PARTITION BY fantoir ORDER BY timestamp_statut DESC,id_statut DESC) rang
-FROM    statut_fantoir),
-a AS
-(SELECT fantoir
-FROM    aa
-WHERE   rang = 1 AND
-        id_statut != 0),
--- Voies de cumul_voies ---------------
-b AS
-(SELECT f.fantoir10
- FROM   fantoir_voie f
- WhERE  f.code_insee LIKE '__dept__%' and type_voie in ('1','2')
+FROM    statut_fantoir
+WHERE   fantoir LIKE '__dept__%') aa
+WHERE   rang = 1         AND
+        id_statut != 0);
+
+-- Fantoirs éligibles ---------------
+CREATE TEMP TABLE liste_fantoir
+AS
+(SELECT f.fantoir10 as fantoir
+FROM    fantoir_voie f
+WHERE   f.code_dept = '__dept__' and type_voie in ('1','2')
+
+INTERSECT
+
+SELECT  fantoir
+FROM    cumul_adresses
+WHERE   source = 'BAN' and dept = '__dept__'
+
 EXCEPT
+
 (SELECT fantoir
- FROM   cumul_voies
- WHERE  insee_com LIKE '__dept__%'
+FROM    cumul_voies
+WHERE   insee_com LIKE '__dept__%'
 UNION
- SELECT fantoir
- FROM   cumul_adresses
- WHERE  insee_com LIKE '__dept__%'  AND
-        (source='OSM' OR (source='BAN' AND COALESCE(voie_osm,'')!='')))),
+SELECT  fantoir
+FROM    statut_fantoir
+UNION
+SELECT  fantoir
+FROM    cumul_adresses
+WHERE   insee_com LIKE '__dept__%'  AND
+        (source='OSM' OR (source='BAN' AND COALESCE(voie_osm,'')!='')))
+);
+                
+--top250
+CREATE TEMP TABLE top250
+as
+SELECT l.fantoir,
+       code_insee,
+       date_creation
+FROM   liste_fantoir l
+JOIN   (SELECT fantoir10 fantoir,
+               code_insee,
+               date_creation
+        FROM   fantoir_voie 
+        WHERE  code_dept = '__dept__') f
+USING   (fantoir)
+ORDER BY date_creation DESC
+LIMIT 250;
+
 -- Géometrie des voies BAN ----
 -- 1 point adresse arbitraire ---------
-c AS
+CREATE TEMP TABLE geom
+AS
 (SELECT DISTINCT fantoir,
         voie_autre,
         FIRST_VALUE(geometrie) OVER(PARTITION BY fantoir) geometrie
-FROM    cumul_adresses
-WHERE   source = 'BAN' AND
-        insee_com LIKE '__dept__%'),
+FROM    (SELECT c.fantoir,
+                voie_autre,
+                geometrie
+        FROM    cumul_adresses c 
+        WHERE   dept = '__dept__' AND
+                source = 'BAN') g
+JOIN    top250
+USING   (fantoir));
+
 -- Assemblage -------------------------
-d AS
-(SELECT f.*,
-        co.libelle commune,
-        co.dep,
-        c.voie_autre,
-        c.geometrie,
-        RANK() OVER(PARTITION BY 1 ORDER BY date_creation DESC,random()) rang
-FROM    (SELECT fantoir10,
-                code_insee,
-                date_creation
-        FROM    fantoir_voie
-        WHERE   code_insee LIKE '__dept__%' AND
-                type_voie IN ('1','2')) f
-JOIN    b
-USING   (fantoir10)
-LEFT OUTER JOIN a
-ON      f.fantoir10 = a.fantoir
-JOIN    c
-ON      f.fantoir10 = c.fantoir
+SELECT  co.dep,
+        f.code_insee,
+        co.libelle,
+        g.voie_autre,
+        f.fantoir,
+        st_x(g.geometrie),
+        st_y(g.geometrie),
+        to_char(to_date(f.date_creation,'YYYYDDD'),'YYYY-MM-DD')
+FROM    top250 f
+JOIN    geom g
+USING   (fantoir)
 JOIN    (SELECT libelle,
                 com,
                 dep
@@ -59,19 +88,4 @@ JOIN    (SELECT libelle,
          WHERE  dep = '__dept__' AND
                 typecom in ('COM','ARM')) co
 ON      co.com = f.code_insee
-WHERE   a.fantoir IS NULL),
--- Selection des 5 1ers par dept-------
-e AS
-(SELECT *
-FROM    d
-WHERE rang < 250)
-SELECT e.dep,
-    e.code_insee,
-    e.commune,
-    e.voie_autre,
-    fantoir10,
-    st_x(e.geometrie),
-    st_y(e.geometrie),
-    to_char(to_date(e.date_creation,'YYYYDDD'),'YYYY-MM-DD')
-FROM    e
-ORDER BY e.date_creation DESC,3,4;
+ORDER BY date_creation DESC,3,4;
