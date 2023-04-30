c44584124eba40fde6601f9c6f45e024129fddfb
Vincent de Château-Thierry <vdct@laposte.net>
2023-04-30 20:56:22 +0000

    Numeros

--- labels_statut_numero.py
+++ labels_statut_numero.py
@@ -1,17 +1,11 @@
 #!./venv37/bin/python
 # -*- coding: utf-8 -*-
 
-import cgi
-import cgitb
 import json
 
-import db
+from sql import sql_get_data
 
-
-cgitb.enable()
 print ("Content-Type: application/json")
 print ("")
 
-with db.bano.cursor() as cur:
-    cur.execute("SELECT id_statut,label_statut FROM labels_statuts_numero ORDER BY tri;")
-    print(json.JSONEncoder().encode(cur.fetchall()))
+print(json.JSONEncoder().encode(sql_get_data('labels_statuts_numeros',{})))
--- sql/inspect_numeros.sql
+++ sql/inspect_numeros.sql
@@ -1,23 +1,38 @@
-SELECT c.source,
-       c.numero,
-       c.voie_osm,
-       c.voie_autre,
+SELECT a.source,
+       a.numero,
+       n.nom,
        COALESCE(s.id_statut,0),
        cog.libelle,
-       c.insee_com,
+       a.code_insee,
        ST_X(geometrie),
        ST_Y(geometrie)
-FROM   cumul_adresses c
+FROM   bano_adresses a
 JOIN   cog_commune cog
-ON     insee_com = com
+ON     code_insee = com
+JOIN   (SELECT fantoir,
+               nom
+        FROM   nom_fantoir
+        WHERE fantoir = '__fantoir__'
+        ORDER BY
+            CASE source
+                WHEN 'OSM' THEN 1
+                ELSE 2
+            END,
+            CASE nature
+                WHEN 'voie' THEN 1
+                WHEN 'lieu-dit' THEN 2
+                ELSE 3
+            END
+        LIMIT 1) n
+USING (fantoir)
 LEFT OUTER JOIN (SELECT numero,fantoir,source,id_statut
                 FROM    (SELECT  *,rank() OVER (PARTITION BY numero,fantoir,source ORDER BY timestamp_statut DESC) rang
                          FROM    statut_numero
-                         WHERE   insee_com = '__com__' AND
+                         WHERE   code_insee = '__code_insee__' AND
                                  fantoir = '__fantoir__')r
                 WHERE   rang = 1) s
 USING (numero,fantoir,source)
-WHERE c.insee_com = '__com__' AND
-      c.fantoir = '__fantoir__' AND
-      c.source = 'BAN'
+WHERE a.code_insee = '__code_insee__' AND
+      a.fantoir = '__fantoir__' AND
+      a.source = 'BAN'
 ORDER BY numero;
