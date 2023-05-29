830a894fbeda1abd5cb1a66e29713020b38d4aef
Vincent de Château-Thierry <vdct@laposte.net>
2023-05-29 14:26:29 +0000

    action refresh

--- refresh.sh
+++ refresh.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-source /data/work/vdct/bano_venv37/bin/activate
+source ./bano_venv37/bin/activate
 
-SCRIPT_DIR=/data/project/bano_prod
+SCRIPT_DIR=/data/project/bano_v3
 cd $SCRIPT_DIR
 
 source config
 
 pip install -qqe .
 
-bano process_commune BAN --code_insee ${1}
-bano process_commune OSM --code_insee ${1}
-bano process_commune_lieux-dits --code_insee ${1}
+bano rapprochement --code_insee ${1}
