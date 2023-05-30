5de24978cc77fa596c71c45a2b86041338db4e60
Vincent de Château-Thierry <vdct@laposte.net>
2023-05-30 20:50:42 +0000

    chemin relatif venv pour refresh

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./bano_venv37/bin/activate
+source ./venv37/bin/activate
 
 SCRIPT_DIR=/data/project/bano_v3
 cd $SCRIPT_DIR
@@ -8,5 +8,4 @@ cd $SCRIPT_DIR
 source config
 
 pip install -qqe .
-
 bano rapprochement --code_insee ${1}
