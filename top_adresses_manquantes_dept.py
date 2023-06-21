43043ab7a851e50e0211e7ea916590ea2771140d
Vincent de Château-Thierry <vdct@laposte.net>
2023-06-21 14:30:10 +0000

    Top adresses manquantes (bis)

--- top_adresses_manquantes_dept.py
+++ top_adresses_manquantes_dept.py
@@ -10,12 +10,10 @@ from sql import sql_get_data
 def format_csv(fetch):
     return ('Département;Code INSEE;Commune;FANTOIR;Voie;Date de création;lon;lat\n'+'\n'.join([f'{c[0]};{c[1]};"{c[2]}";{c[4]};{c[3]};{c[7]};{c[5]};{c[6]}' for c in fetch]))
 
-# params = cgi.FieldStorage()
-# dept = params['dept'].value
+params = cgi.FieldStorage()
+dept = params['dept'].value
+format = params.getvalue('format','json')
 
-dept = '40'
-# format = params.getvalue('format','json')
-format = 'json'
 if format == 'json':
     print("Content-Type: application/json\n")
     print(json.JSONEncoder().encode(sql_get_data('top_adresses_manquantes_dept',{'dept':dept})))
