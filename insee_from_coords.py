dbf0cf4d657f3e315d544cb7c215bd92f4db9b7a
Vincent de Château-Thierry <vdct@laposte.net>
2023-07-01 22:11:39 +0000

    Appel depuis JOSM

--- insee_from_coords.py
+++ insee_from_coords.py
@@ -3,24 +3,13 @@
 
 import cgi
 import cgitb
-import os.path
-import sys
 import json
 
-import db
-
-def get_data(lon,lat):
-    dbconn = db.bano_cache
-
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'sql/insee_from_coords.sql'),'r') as fq:
-        with dbconn.cursor() as cur:
-            cur.execute(fq.read().replace('__lon__',lon).replace('__lat__',lat))
-
-            return cur.fetchone()
+from sql import sql_get_data
 
 params = cgi.FieldStorage()
 lon = params['lon'].value
 lat = params['lat'].value
 
 print("Content-Type: application/json\n")
-print(json.JSONEncoder().encode(get_data(lon,lat)))
+print(json.JSONEncoder().encode(sql_get_data('insee_from_coords',{'lon':str(lon),'lat':str(lat)})))
--- sql/insee_from_coords.sql
+++ sql/insee_from_coords.sql
@@ -1,19 +1,19 @@
 WITH
 s
 AS
-(SELECT 	insee_com,
-CASE typecom
-  WHEN 'ARM' THEN 1
-  WHEN 'COM' THEN 2
-  ELSE 3
-END sort_order
-FROM	polygones_insee_geo
-JOIN    cog_commune c
-ON      (com = insee_com)
-WHERE	ST_Contains(geometrie,ST_SetSRID(ST_Point(__lon__,__lat__),4326))
+(SELECT   code_insee,
+          CASE typecom
+            WHEN 'ARM' THEN 1
+            WHEN 'COM' THEN 2
+            ELSE 3
+          END sort_order
+FROM      polygones_insee
+JOIN      cog_commune c
+ON        (com = code_insee)
+WHERE     ST_Contains(geometrie,ST_SetSRID(ST_Point(__lon__,__lat__),4326))
 UNION ALL
-SELECT '404',99)
-SELECT insee_com
-FROM s
-ORDER BY sort_order
+SELECT    '404',99)
+SELECT    code_insee
+FROM      s
+ORDER BY  sort_order
 LIMIT 1;
