ad4731eb139d2a908e9aabee1a468cf6ec224a0d
Vincent de Château-Thierry <vdct@laposte.net>
2023-07-03 19:29:27 +0000

    Nom des communes & code dept  pour les Fantoir annulés

--- sql/fantoir_annule.sql
+++ sql/fantoir_annule.sql
@@ -2,12 +2,20 @@ SELECT DISTINCT p.fantoir,
        nom,
        ST_X(geometrie),
        ST_Y(geometrie),
-       date_annul
+       date_annul,
+       dep,
+       libelle
 FROM   bano_points_nommes p
 JOIN   (SELECT  fantoir,
                 TO_CHAR(TO_DATE(date_annul::text,'YYYYMMDD'),'YYYY-MM-DD') date_annul
 FROM   topo
 WHERE  date_annul!= 0)f
 USING  (fantoir)
+JOIN   (SELECT dep,
+               com AS code_insee,
+               libelle
+       FROM cog_commune
+       WHERE typecom = 'COM') c
+USING  (code_insee)
 ORDER  BY 1
 
