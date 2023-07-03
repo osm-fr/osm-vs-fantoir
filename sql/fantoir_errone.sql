028c6260ef3726bd021431f7d7a66a4b78ce6d73
Vincent de Château-Thierry <vdct@laposte.net>
2023-07-03 10:26:26 +0000

    Nom des communes pour les Fantoir erronés

--- sql/fantoir_errone.sql
+++ sql/fantoir_errone.sql
@@ -1,19 +1,26 @@
 WITH
 f
 AS
-(SELECT fantoir
+(SELECT code_insee,
+        fantoir
  FROM   bano_points_nommes
  WHERE  fantoir IS NOT NULL AND
         source = 'OSM' AND
         fantoir not like '%b'
 EXCEPT
-SELECT  fantoir
+SELECT  code_insee,
+        fantoir
 FROM	 topo)
 SELECT DISTINCT v.fantoir,
        nom,
        ST_X(geometrie),
-       ST_Y(geometrie)
+       ST_Y(geometrie),
+       libelle
 FROM   bano_points_nommes v
+JOIN   (SELECT com AS code_insee,
+               libelle
+       FROM cog_commune) c
+USING  (code_insee)
 JOIN   f
 USING  (fantoir)
 WHERE  source = 'OSM'
