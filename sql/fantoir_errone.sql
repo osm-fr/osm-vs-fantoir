431fee3f9c664a04f57488c73b0775b3f453f069
Vincent de Château-Thierry <vdct@laposte.net>
2023-07-03 11:01:08 +0000

    Nom des communes & code dept  pour les Fantoir erronés

--- sql/fantoir_errone.sql
+++ sql/fantoir_errone.sql
@@ -15,11 +15,14 @@ SELECT DISTINCT v.fantoir,
        nom,
        ST_X(geometrie),
        ST_Y(geometrie),
+       dep,
        libelle
 FROM   bano_points_nommes v
-JOIN   (SELECT com AS code_insee,
+JOIN   (SELECT dep,
+              com AS code_insee,
                libelle
-       FROM cog_commune) c
+       FROM cog_commune
+       WHERE typecom = 'COM') c
 USING  (code_insee)
 JOIN   f
 USING  (fantoir)
