e7cbae45b4a675d54e1ff4c80d7389cdebc34a86
Vincent de Château-Thierry <vdct@laposte.net>
2023-09-03 20:47:00 +0000

    WIP Rendu BANO

--- numeros_pour_rendu_commune.py
+++ numeros_pour_rendu_commune.py
@@ -10,9 +10,13 @@ import json
 # import db
 from sql import sql_get_data
 
-# params = cgi.FieldStorage()
-# insee = params['insee'].value
-insee = '87187'
+params = cgi.FieldStorage()
+insee = params['insee'].value
+# insee = '87187'
+
+data = sql_get_data('rendu_bano_polygones',{'code_insee':insee})
+jdata = [[d[0],d[1],json.loads(d[2])] for d in data]
 
 print("Content-Type: application/json\n")
-print(json.JSONEncoder().encode(sql_get_data('rendu_bano_polygones',{'code_insee':insee})))
+# print(json.JSONEncoder().encode(sql_get_data('rendu_bano_polygones',{'code_insee':insee})))
+print(json.JSONEncoder().encode(jdata))
