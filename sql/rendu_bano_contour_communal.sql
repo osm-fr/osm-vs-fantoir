428903b61a388b0ded97342c198a29a3f289d1c4
Vincent de Château-Thierry <vdct@laposte.net>
2023-10-06 22:33:58 +0000

    contour carto des arrondissements

--- sql/rendu_bano_contour_communal.sql
+++ sql/rendu_bano_contour_communal.sql
@@ -1,10 +1,13 @@
 WITH
 p
 AS
-(SELECT geometrie
+(SELECT geometrie,
+        admin_level
 FROM    polygones_insee
 WHERE   code_insee = '__code_insee__' AND
-        admin_level = 8)
+        admin_level in (8,9)
+ORDER BY admin_level ASC
+LIMIT 1)
 SELECT ST_AsGeoJSON(ST_BoundingDiagonal(geometrie))
 FROM p
 UNION ALL
