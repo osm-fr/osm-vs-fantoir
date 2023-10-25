002186b472e6d46cc865953dba0140e0dd989f83
Vincent de Château-Thierry <vdct@laposte.net>
2023-10-25 21:29:32 +0000

    arrivee des stats par commune

--- requete_pifometre_commune.py
+++ requete_pifometre_commune.py
@@ -13,26 +13,58 @@ from sql import sql_get_data
 cgitb.enable()
 
 params = cgi.FieldStorage()
-code_insee = params['insee'].value
+code_insee = params["insee"].value
 
 nom_commune = []
 lon_commune = None
 lat_commune = None
 date_debut = None
 commune_composee = None
-infos_commune = sql_get_data('infos_commune_insee',{'code_insee':code_insee})
+infos_commune = sql_get_data("infos_commune_insee", {"code_insee": code_insee})
 if infos_commune:
-    nom_commune,lon_commune,lat_commune,date_debut,commune_composee,bounds = infos_commune[0]
+    (
+        nom_commune,
+        lon_commune,
+        lat_commune,
+        date_debut,
+        commune_composee,
+        bounds,
+        adresses_osm,
+        adresses_ban,
+        nom_osm,
+        nom_ban,
+        nom_cadastre,
+        nom_topo,
+    ) = infos_commune[0]
 
 insee_commune_parente = None
 nom_commune_parente = None
-commune_parente = sql_get_data('commune_parente',{'code_insee':code_insee})
+commune_parente = sql_get_data("commune_parente", {"code_insee": code_insee})
 if commune_parente:
     insee_commune_parente, nom_commune_parente = commune_parente[0]
 
-a_voisins = [[v[0],v[1],v[2]] for v in sql_get_data('voisins_insee',{'code_insee':code_insee})]
+a_voisins = [
+    [v[0], v[1], v[2]]
+    for v in sql_get_data("voisins_insee", {"code_insee": code_insee})
+]
 
-data = [nom_commune,lon_commune,lat_commune,a_voisins,insee_commune_parente, nom_commune_parente,date_debut,commune_composee,json.loads(bounds)]
+data = [
+    nom_commune,
+    lon_commune,
+    lat_commune,
+    a_voisins,
+    insee_commune_parente,
+    nom_commune_parente,
+    date_debut,
+    commune_composee,
+    json.loads(bounds),
+    adresses_osm,
+    adresses_ban,
+    nom_osm,
+    nom_ban,
+    nom_cadastre,
+    nom_topo,
+]
 
-print('Content-Type: application/json\n')
+print("Content-Type: application/json\n")
 print(json.JSONEncoder().encode(data))
--- sql/infos_commune_insee.sql
+++ sql/infos_commune_insee.sql
@@ -1,20 +1,30 @@
-SELECT nom,
+SELECT  nom,
         ST_X(p),
         ST_Y(p),
         COALESCE(date_debut,'01-10-1970'),
         commune_composee,
-        json_bounds
+        json_bounds,
+        nb_adresses_osm,
+        nb_adresses_ban,
+        nb_nom_osm,
+        nb_nom_ban,
+        nb_nom_cadastre,
+        nb_nom_topo
 FROM    (SELECT nom,
                 ST_AsGeoJSON(ST_BoundingDiagonal(geometrie)) json_bounds,
                 ST_Centroid(geometrie) p,
                 admin_level,
+                code_insee,
                 code_insee AS code_zone
         FROM    polygones_insee
         WHERE   admin_level in (8,9) AND
                 code_insee = '__code_insee__'
         ORDER BY admin_level
         LIMIT 1)a
-LEFT OUTER JOIN  (SELECT code_zone,date_debut
+JOIN    bano_stats_communales
+USING   (code_insee)
+LEFT OUTER JOIN  (SELECT code_zone,
+                         date_debut
                  FROM    batch
                  WHERE   code_zone = '__code_insee__' AND
                          etape = 'rapprochement') r
