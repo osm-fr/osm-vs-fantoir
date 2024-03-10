6e41104dff931ed8dbffa0a5c4ee2c25dfb1bfb9
Vincent de Château-Thierry <vdct@laposte.net>
2024-03-10 21:57:12 +0000

    Distinction entre rapprochements BANO et Pifometre

--- refresh.sh
+++ refresh.sh
@@ -5,4 +5,4 @@ source /data/work/vdct/bano_venv_v3/bin/activate
 SCRIPT_DIR=/data/project/bano_v3
 cd $SCRIPT_DIR
 source config
-bano rapprochement --code_insee ${1}
+bano rapprochement --code_insee ${1} -p
