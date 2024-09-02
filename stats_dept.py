f7a4438f422f5c9386e5c68d49cb53630a986a1c
Vincent de Château-Thierry <vdct@laposte.net>
2024-09-02 21:47:16 +0000

    Nom de colonne manquant dans le CSV (fix #412)

--- stats_dept.py
+++ stats_dept.py
@@ -10,7 +10,7 @@ from sql import sql_get_data
 
 
 def format_csv(fetch):
-    return ('Code INSEE;Commune;Voies avec adresses rapprochées (a);Toutes voies rapprochées (b)...;...dont voies rapprochées sur lieux-dits FANTOIR;Voies FANTOIR (c);Voies et lieux-dits FANTOIR (d);Voies avec adresses voies FANTOIR (a/c) en %;Voies rapprochées voies FANTOIR (b/c) en %;Voies rapprochées voies et lieux-dits FANTOIR (b/d) en %;Adresses OSM;Adresses BAN;Adresses BAN sans voie rapprochées;Adresse BAN avec voie rapprochée en %;Indice 2020\n'+'\n'.join([f'{c[0]};{c[1]};{c[2]};{c[3]};{c[4]};{c[5]};{c[6]};{c[7]};{c[8]};{c[9]};{c[10]};{c[11]};{c[12]};{c[13]};{c[14]};{c[15]}' for c in fetch]))
+    return ('Code INSEE;Commune;Pct adresses certifiées BAN;Voies avec adresses rapprochées (a);Toutes voies rapprochées (b)...;...dont voies rapprochées sur lieux-dits FANTOIR;Voies FANTOIR (c);Voies et lieux-dits FANTOIR (d);Voies avec adresses voies FANTOIR (a/c) en %;Voies rapprochées voies FANTOIR (b/c) en %;Voies rapprochées voies et lieux-dits FANTOIR (b/d) en %;Adresses OSM;Adresses BAN;Adresses BAN sans voie rapprochées;Adresse BAN avec voie rapprochée en %;Indice 2020\n'+'\n'.join([f'{c[0]};{c[1]};{c[2]};{c[3]};{c[4]};{c[5]};{c[6]};{c[7]};{c[8]};{c[9]};{c[10]};{c[11]};{c[12]};{c[13]};{c[14]};{c[15]}' for c in fetch]))
 
 cgitb.enable()
 params = cgi.FieldStorage()
