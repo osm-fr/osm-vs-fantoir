5c39c3b09d4f231cce8da8fbe4152b316420f450
Vincent de Château-Thierry <vdct@laposte.net>
2024-09-26 20:44:28 +0000

    croisements - cron differentiel

---
+++ cron_croisement.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+source /data/work/vdct/bano_venv_v3/bin/activate
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd /data/project/bano_v3
+source config
+
+pip install -qe .
+
+cd $SCRIPT_DIR
+
+LOCKFILE=${SCRIPT_DIR}/croisement.lock
+INDEXFILE=${SCRIPT_DIR}/imposm_line.index
+DEPTFILE=${SCRIPT_DIR}/depts_croisement.csv
+TILESFILE=${SCRIPT_DIR}/tiles_croisement.csv
+LOGFILE=${SCRIPT_DIR}/cron_croisement.log
+
+echo `date`>> ${LOGFILE}
+echo debut >> ${LOGFILE}
+
+if test -f ${LOCKFILE}
+then
+  diff_age=$((`date +%s` - `stat -c %Y $LOCKFILE`))
+  if [ $diff_age -gt 7200 ];then
+    echo "Effacement du lock" >> ${LOGFILE}
+    rm ${LOCKFILE}
+  else
+    echo `date`" : Process deja en cours" >> ${LOGFILE}
+    exit 0
+  fi
+fi
+
+# utile à l'initialisation
+if test ! -f ${INDEXFILE}
+then
+    $pgsql_BANO --csv -t -c "SELECT last_value - 1 FROM planet_osm_line_id_seq" > ${INDEXFILE}
+    echo ${pgsql_BANO}
+fi
+
+lastindex=`cat ${INDEXFILE}`
+
+echo "last index : ${lastindex}" >> $LOGFILE
+
+# En prévision de la prochaine passe
+$pgsql_BANO --csv -t -c "SELECT last_value FROM planet_osm_line_id_seq" > ${INDEXFILE}
+
+touch ${LOCKFILE}
+
+# Département des communes où des modifs ont eu lieu dans planet_osm_line depuis la dernière passe
+$pgsql_BANO --csv -t -c "SELECT distinct dep
+                         FROM (SELECT way FROM planet_osm_line WHERE id >= ${lastindex}) l
+                               JOIN   polygones_insee p
+                               ON     way && geometrie
+                               JOIN   cog_commune c
+                               ON     com = code_insee
+                               WHERE  dep != ''
+                               ORDER BY 1" > ${DEPTFILE}
+
+# backup du dernier état de la table croisement_voies_limites pour comparaison ultérieure et détermination des tuiles à raffraichir
+$pgsql_BANO -c "DROP TABLE IF EXISTS croisement_voies_limites_precedent CASCADE;
+                CREATE TABLE croisement_voies_limites_precedent AS SELECT geometrie_osm,id FROM croisement_voies_limites;"
+
+cd ${BANO_DIR}
+parallel -j $PARALLEL_JOBS -a ${DEPTFILE} bano croisement_voies_limites {1}
+echo `wc -l ${DEPTFILE}` "départements traités" >> ${LOGFILE}
+cd -
+
+rm ${TILESFILE}
+for zoom in {5..12}
+do
+   psql -d bano -U cadastre --csv -t -c "SELECT ${zoom}||' '||
+                                                lon2tile(ST_X(ST_StartPoint(c.geometrie_osm)),${zoom})||' '|| 
+                                                lat2tile(ST_Y(ST_StartPoint(c.geometrie_osm)),${zoom})
+                                         FROM   croisement_voies_limites c
+                                         LEFT JOIN croisement_voies_limites_precedent p
+                                         USINg (id)
+                                         WHERE  p.id IS NULL
+                                         UNION
+                                         SELECT ${zoom}||' '||
+                                                lon2tile(ST_X(ST_EndPoint(c.geometrie_osm)),${zoom})||' '|| 
+                                                lat2tile(ST_Y(ST_EndPoint(c.geometrie_osm)),${zoom})
+                                         FROM   croisement_voies_limites c
+                                         LEFT JOIN croisement_voies_limites_precedent p
+                                         USINg (id)
+                                         WHERE  p.id IS NULL
+                                         UNION
+                                         SELECT ${zoom}||' '||
+                                                lon2tile(ST_X(ST_StartPoint(p.geometrie_osm)),${zoom})||' '|| 
+                                                lat2tile(ST_Y(ST_StartPoint(p.geometrie_osm)),${zoom})
+                                         FROM   croisement_voies_limites_precedent p
+                                         LEFT JOIN croisement_voies_limites c
+                                         USINg (id)
+                                         WHERE  c.id IS NULL
+                                         UNION
+                                         SELECT ${zoom}||' '||
+                                                lon2tile(ST_X(ST_EndPoint(p.geometrie_osm)),${zoom})||' '|| 
+                                                lat2tile(ST_Y(ST_EndPoint(p.geometrie_osm)),${zoom})
+                                         FROM   croisement_voies_limites_precedent p
+                                         LEFT JOIN croisement_voies_limites c
+                                         USINg (id)
+                                         WHERE  c.id IS NULL
+                                         ORDER BY 1" >> ${TILESFILE}
+done
+
+parallel -a ${TILESFILE} --colsep ' ' -j 4 ./generate-tiles_croisement.sh {1} {2} {3}
+
+rm ${LOCKFILE}
+
+echo `wc -l ${TILESFILE}` "tuiles produites" >> ${LOGFILE}
+echo `date` >> ${LOGFILE}
+echo fin >> ${LOGFILE}
--- generate-tiles_croisement.sh
+++ generate-tiles_croisement.sh
@@ -5,13 +5,13 @@
 
 set -e
 
-function croisement_voies_limites() {
+function cvl_voie() {
   tz=$1
   tx=$2
   ty=$3
   echo "
   COPY (
-SELECT ST_AsMVT(q, 'croisement_voies_limites', 4096, 'geom')
+SELECT ST_AsMVT(q, 'cvl_voie', 4096, 'geom')
 FROM ( SELECT osm_id,
               nom_osm,
               nom_commune_debut,
@@ -31,11 +31,94 @@ FROM ( SELECT osm_id,
   "
 }
 
+function cvl_point_debut() {
+  tz=$1
+  tx=$2
+  ty=$3
+  echo "
+  COPY (
+SELECT ST_AsMVT(q, 'cvl_point_debut', 4096, 'geom')
+FROM ( SELECT osm_id,
+              nom_osm,
+              nom_commune_debut,
+              code_insee_debut,
+              nom_commune_fin,
+              code_insee_fin,
+              rapproche_debut,
+              ST_AsMvtGeom(
+                point_debut_3857,
+                BBox($tx, $ty, $tz),
+                4096,
+                256,
+                true
+              ) AS geom
+       FROM croisement_voies_limites
+    ) AS q
+  ) TO STDOUT;
+  "
+}
+
+function cvl_point_fin() {
+  tz=$1
+  tx=$2
+  ty=$3
+  echo "
+  COPY (
+SELECT ST_AsMVT(q, 'cvl_point_fin', 4096, 'geom')
+FROM ( SELECT osm_id,
+              nom_osm,
+              nom_commune_debut,
+              code_insee_debut,
+              nom_commune_fin,
+              code_insee_fin,
+              rapproche_fin,
+              ST_AsMvtGeom(
+                point_fin_3857,
+                BBox($tx, $ty, $tz),
+                4096,
+                256,
+                true
+              ) AS geom
+       FROM croisement_voies_limites
+    ) AS q
+  ) TO STDOUT;
+  "
+}
+
+function cvl_point_croisement() {
+  tz=$1
+  tx=$2
+  ty=$3
+  echo "
+  COPY (
+SELECT ST_AsMVT(q, 'cvl_point_croisement', 4096, 'geom')
+FROM ( SELECT osm_id,
+              nom_osm,
+              code_insee_debut,
+              code_insee_fin,
+              ST_AsMvtGeom(
+                geometrie_3857,
+                BBox($tx, $ty, $tz),
+                4096,
+                256,
+                true
+              ) AS geom
+       FROM point_croisement_voies_limites
+    ) AS q
+  ) TO STDOUT;
+  "
+}
+
 z=$1
 x=$2
 y=$3
 
 mkdir -p ./tiles_croisement/$z/$x
 ofile="tiles_croisement/$z/$x/$y.pbf"
-psql -d bano -U cadastre -tq -c "$(croisement_voies_limites $z $x $y)" | xxd -r -p  > $ofile
+{
+  psql -d bano -U cadastre -tq -c "$(cvl_voie $z $x $y)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_point_debut $z $x $y)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_point_fin $z $x $y)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_point_croisement $z $x $y)" | xxd -r -p ;
+} > $ofile
 echo $ofile
