827165f3f508c0d8eba56e5d1cc00f316fc21f21
Vincent de Château-Thierry <vdct@laposte.net>
2024-10-27 22:08:03 +0000

    Pifodrome : dezoom à l'arrivée des dés

--- sql/bilto.sql
+++ sql/bilto.sql
@@ -6,7 +6,7 @@ GROUP BY 1
 HAVING count(*) = 1),
 r
 AS
-(SELECT ST_AsGeoJSON(ST_BoundingDiagonal(geometrie_osm)) json_bounds,
+(SELECT ST_BoundingDiagonal(geometrie_osm) AS diagonale,
         ST_Distance(point_debut_3857,geometrie_3857)::integer dd,
         ST_Distance(point_fin_3857,geometrie_3857)::integer df
 FROM    croisement_voies_limites c
@@ -16,7 +16,7 @@ JOIN    point_croisement_voies_limites
 USING   (osm_id)
 WHERE   (__not_debut__ rapproche_debut AND __not_fin__ rapproche_fin) OR
         (__not_fin__ rapproche_debut AND __not_debut__ rapproche_fin))
-SELECT  json_bounds
+SELECT  ST_AsGeoJSON(ST_BoundingDiagonal(ST_Buffer(diagonale,0.0015,2)))
 FROM    r
 WHERE   dd > 200 AND
         df > 200
