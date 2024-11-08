554e7747b2cbdec372da66940505a2ca698b2909
Vincent de Château-Thierry <vdct@laposte.net>
2024-11-08 22:09:41 +0000

    Attribut petit_chevauchement dans les tuiles

--- cron_croisement.sh
+++ cron_croisement.sh
@@ -16,6 +16,7 @@ INDEXFILE=${SCRIPT_DIR}/osm2pgsql_line.index
 DEPTFILE=${SCRIPT_DIR}/depts_croisement.csv
 TILESFILE=${SCRIPT_DIR}/tiles_croisement.csv
 LOGFILE=${SCRIPT_DIR}/cron_croisement.log
+DISTANCE_PETIT_CHEVAUCHEMENT=1
 
 echo `date`>> ${LOGFILE}
 echo debut >> ${LOGFILE}
@@ -114,7 +115,7 @@ do
                                          ORDER BY 1" >> ${TILESFILE}
 done
 
-parallel -a ${TILESFILE} --colsep ' ' -j 4 ./generate-tiles_croisement.sh {1} {2} {3}
+parallel -a ${TILESFILE} --colsep ' ' -j 4 ./generate-tiles_croisement.sh {1} {2} {3} ${DISTANCE_PETIT_CHEVAUCHEMENT}
 
 rm ${LOCKFILE}
 
--- generate-tiles_croisement.sh
+++ generate-tiles_croisement.sh
@@ -5,12 +5,13 @@
 
 set -e
 
-RACINE_CIBLE=tiles_croisement_240929
+RACINE_CIBLE=tiles_croisement_241107
 
 function cvl_voie() {
   tz=$1
   tx=$2
   ty=$3
+  d=$4
   echo "
   COPY (
 SELECT ST_AsMVT(q, 'cvl_voie', 4096, 'geom')
@@ -20,6 +21,7 @@ FROM ( SELECT osm_id,
               code_insee_debut,
               nom_commune_fin,
               code_insee_fin,
+              (plus_petit_chevauchement < $d)::boolean AS ppc,
               ST_AsMvtGeom(
                 geometrie_osm_3857,
                 BBox($tx, $ty, $tz),
@@ -37,6 +39,7 @@ function cvl_point_debut() {
   tz=$1
   tx=$2
   ty=$3
+  d=$4
   echo "
   COPY (
 SELECT ST_AsMVT(q, 'cvl_point_debut', 4096, 'geom')
@@ -47,6 +50,7 @@ FROM ( SELECT osm_id,
               nom_commune_fin,
               code_insee_fin,
               rapproche_debut,
+              (plus_petit_chevauchement < $d)::boolean AS ppc,
               ST_AsMvtGeom(
                 point_debut_3857,
                 BBox($tx, $ty, $tz),
@@ -64,6 +68,7 @@ function cvl_point_fin() {
   tz=$1
   tx=$2
   ty=$3
+  d=$4
   echo "
   COPY (
 SELECT ST_AsMVT(q, 'cvl_point_fin', 4096, 'geom')
@@ -74,6 +79,7 @@ FROM ( SELECT osm_id,
               nom_commune_fin,
               code_insee_fin,
               rapproche_fin,
+              (plus_petit_chevauchement < $d)::boolean AS ppc,
               ST_AsMvtGeom(
                 point_fin_3857,
                 BBox($tx, $ty, $tz),
@@ -91,6 +97,7 @@ function cvl_point_croisement() {
   tz=$1
   tx=$2
   ty=$3
+  d=$4
   echo "
   COPY (
 SELECT ST_AsMVT(q, 'cvl_point_croisement', 4096, 'geom')
@@ -99,6 +106,7 @@ FROM ( SELECT osm_id,
               code_insee_debut,
               code_insee_fin,
               degres,
+              (plus_petit_chevauchement < $d)::boolean AS ppc,
               ST_AsMvtGeom(
                 geometrie_3857,
                 BBox($tx, $ty, $tz),
@@ -115,13 +123,14 @@ FROM ( SELECT osm_id,
 z=$1
 x=$2
 y=$3
+d=$4
 
 mkdir -p ./${RACINE_CIBLE}/$z/$x
 ofile="${RACINE_CIBLE}/$z/$x/$y.pbf"
 {
-  psql -d bano -U cadastre -tq -c "$(cvl_voie $z $x $y)" | xxd -r -p ;
-  psql -d bano -U cadastre -tq -c "$(cvl_point_debut $z $x $y)" | xxd -r -p ;
-  psql -d bano -U cadastre -tq -c "$(cvl_point_fin $z $x $y)" | xxd -r -p ;
-  psql -d bano -U cadastre -tq -c "$(cvl_point_croisement $z $x $y)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_voie $z $x $y $d)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_point_debut $z $x $y $d)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_point_fin $z $x $y $d)" | xxd -r -p ;
+  psql -d bano -U cadastre -tq -c "$(cvl_point_croisement $z $x $y $d)" | xxd -r -p ;
 } > $ofile
 echo $ofile
