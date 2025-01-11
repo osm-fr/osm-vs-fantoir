60fe333f56138d64bdbaaf05deed70b6450c7798
Vincent de Château-Thierry <vdct@laposte.net>
2025-01-11 22:59:42 +0000

    Pifomap : LD grisés si avec statut

--- pifometre/sql/rendu_bano_points.sql
+++ pifometre/sql/rendu_bano_points.sql
@@ -1,4 +1,15 @@
 WITH
+latest_statut
+AS
+(SELECT fantoir,
+        label_statut
+FROM    (SELECT *,
+                RANK() OVER(PARTITION BY fantoir ORDER BY timestamp_statut DESC,id_statut DESC) rang
+        FROM    statut_fantoir)f
+JOIN    labels_statuts_fantoir
+USING   (id_statut)
+WHERE   rang = 1 AND
+        id_statut != 0),
 ban
 AS
 (SELECT DISTINCT fantoir
@@ -10,6 +21,7 @@ SELECT nom,
        fantoir,
        lon,
        lat,
+       label_statut,
        'Co'|| -- C = source Cadastre - o = inconnu d'OSM
        CASE
            WHEN ban.fantoir IS NULL THEN 'b'
@@ -27,6 +39,8 @@ LEFT OUTER JOIN (SELECT DISTINCT fantoir
 USING (fantoir)
 LEFT OUTER JOIN ban
 USING (fantoir)
+LEFT OUTER JOIN latest_statut
+USING (fantoir)
 WHERE o.fantoir IS NULL
 UNION ALL
 -- Voies & LD OSM
@@ -34,6 +48,7 @@ SELECT nom,
        fantoir,
        lon,
        lat,
+       label_statut,
        CASE
            WHEN fantoir IS NULL THEN 'Of' -- O = source OSM - f = sans code Fantoir
            ELSE 'OF'                      -- O = source OSM - F = avec code Fantoir
@@ -53,6 +68,8 @@ FROM   (SELECT *
               nature IN ('place','centroide') AND
               source = 'OSM') o
 LEFT OUTER JOIN ban
+USING (fantoir)
+LEFT OUTER JOIN latest_statut
 USING (fantoir)
 -- on exclut les rues rapprochées avec adresses
 WHERE NOT (nature = 'centroide' AND ban.fantoir IS NOT NULL)
