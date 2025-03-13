f356f08c9a8918c152a33ece855b63368eb869ab
Vincent de Château-Thierry <vdct@laposte.net>
2025-03-13 16:13:49 +0000

    Adresses : priorité au nom du tag name dans les relations

--- pifometre/sql/name_and_positions_OSM_as_GeoJSON.sql
+++ pifometre/sql/name_and_positions_OSM_as_GeoJSON.sql
@@ -5,12 +5,18 @@ JOIN    (SELECT fantoir,
 	            nom
 	     FROM   nom_fantoir
 	     WHERE  fantoir = '__fantoir__'
-	     ORDER BY 
+	     ORDER BY
              CASE source
                  WHEN 'OSM' THEN 1
                  WHEN 'BAN' THEN 2
                  WHEN 'CADASTRE' THEN 3
                  ELSE  4
+             END,
+             CASE nom_tag
+                 WHEN 'name' THEN 1
+                 WHEN 'alt_name' THEN 2
+                 WHEN 'old_name' THEN 4
+                 ELSE  3
              END,
              nature
          LIMIT 1) AS nom
