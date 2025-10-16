932ed487d9b51df2e0e11bdd93c414e67c329f1b
Vincent de Château-Thierry <vdct@laposte.net>
2025-10-16 21:17:53 +0000

    Masquage des nouveaux pseudo-Fantoir (Cadastre et BD Topo)

--- pifometre/sql/listing_fantoir.sql
+++ pifometre/sql/listing_fantoir.sql
@@ -20,5 +20,5 @@ USING (fantoir)
 LEFT OUTER JOIN a
 USING (fantoir)
 WHERE	code_insee = '__code_insee__' AND
-        COALESCE(caractere_annul,'x') != 'B' -- Filtre sur les pseudo-Fantoir BAN
-ORDER BY 4; 
+        COALESCE(caractere_annul,'x') NOT IN ('B','C','T') -- Filtre sur les pseudo-Fantoir BAN / Cadastre / BDTopo
+ORDER BY 4;
